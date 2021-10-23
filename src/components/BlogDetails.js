c90fa6e6e1e526bbd61c86012ac42a6edee3169e
bthank <bthankachan@yahoo.com>
2021-10-23 19:38:46 -0400

    Deleting blogs changes

--- src/components/BlogDetails.js
+++ src/components/BlogDetails.js
@@ -1,5 +1,6 @@
-import { useParams } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
 import useFetch from "../hooks/useFetch";
+ 
 
 const BlogDetails = () => {
 
@@ -8,7 +9,16 @@ const BlogDetails = () => {
     const { id } = useParams();
     // call useFetch to return the data and other 2 properties (error and isPending) for the specific blog to display
     const { data: blog, error, isPending } = useFetch('http://localhost:8000/blogs/' + id);
-     
+    const history = useHistory();
+
+    const handleClick = () => {
+        fetch('http://localhost:8000/blogs/' + blog.id, {
+            method: 'DELETE'
+        })
+        .then(() => {
+            history.push('/');
+        });
+    };
 
     return ( 
         <div className="blog-details">
@@ -19,6 +29,7 @@ const BlogDetails = () => {
                     <h2>{ blog.title }</h2>
                     <p>Written by {blog.author}</p>
                     <div>{blog.body}</div>
+                    <button onClick={handleClick}>Delete</button>
                 </article>
                 
             )}
