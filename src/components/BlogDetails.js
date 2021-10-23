b0a347aa68458c33bdf9e0341ae969252e0b77dc
bthank <bthankachan@yahoo.com>
2021-10-23 18:36:01 -0400

    updated BlogDetails component and styled it

--- src/components/BlogDetails.js
+++ src/components/BlogDetails.js
@@ -1,14 +1,27 @@
 import { useParams } from "react-router-dom";
+import useFetch from "../hooks/useFetch";
 
 const BlogDetails = () => {
 
     // useParams is found in react-router as well; MAKE SURE you pick it from 'react-router-dom'
     // the useParams hook gives us access to route parameters
     const { id } = useParams();
+    // call useFetch to return the data and other 2 properties (error and isPending) for the specific blog to display
+    const { data: blog, error, isPending } = useFetch('http://localhost:8000/blogs/' + id);
+     
 
     return ( 
         <div className="blog-details">
-            <h2>Blog Details - {id}</h2>
+            { isPending && <div>Loading ...</div>}
+            { error && <div>{error}</div>}
+            {blog && (
+                <article>
+                    <h2>{ blog.title }</h2>
+                    <p>Written by {blog.author}</p>
+                    <div>{blog.body}</div>
+                </article>
+                
+            )}
         </div>
      );
 }
