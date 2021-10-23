7fb66597980c7927e2a234faa503d6cd6c30ef64
bthank <bthankachan@yahoo.com>
2021-10-23 17:57:19 -0400

    added Link components

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,17 @@
+import { Link } from 'react-router-dom';
+
 const Navbar = () => {
     return ( 
         <nav className="navbar">
             <h1>The Dojo Blog</h1>
             <div className="links">
-                <a href="/">Home</a>
-                <a href="/create" style={{
+                {/* use Link instead of a href tags in order to prevent url requests from going to the server and allow React front end to handle the routing */}
+                <Link to="/">Home</Link>
+                <Link to="/create" style={{
                     color: 'white',
                     backgroundColor: '#f1356d',
                     borderRadius: '8px'
-                }}>New Blog</a>
+                }}>New Blog</Link>
             </div>
         </nav>
      );
