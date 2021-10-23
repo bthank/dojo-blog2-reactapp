2c2092ddc517c73d39bd7affb516a48ed82aecbf
bthank <bthankachan@yahoo.com>
2021-10-23 16:54:28 -0400

    conditional rendering of bloglist after obtaining data from json-server via fetch request

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -2,7 +2,7 @@
 //const BlogList = (props) => {
 //    const blogs = props.blogs;
 //    const title = props.title;
-const BlogList = ({blogs, title, handleDelete}) => {
+const BlogList = ({blogs, title}) => {
 
 
 
@@ -13,7 +13,6 @@ const BlogList = ({blogs, title, handleDelete}) => {
                 <div className="blog-preview" key={blog.id}>
                     <h2>{blog.title}</h2>
                     <p>Written by {blog.author}</p>
-                    <button onClick={() => handleDelete(blog.id)}>Delete Blog</button>
                 </div>
             ))}
         </div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -27,23 +27,21 @@ const Home = () => {
         </div>
      );
 */
-    const [blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-    ]);
-    const [name,setName] = useState('Mario');
-
-    const handleDelete = (id) => {
-        const newBlogList = blogs.filter((blog) => blog.id !== id);
-        setBlogs(newBlogList);
-    };
+    const [blogs, setBlogs] = useState(null);
 
     // useEffect fires on every render; note that a change of state triggers a render
     useEffect(() => {
         console.log('useEffect ran');
-        console.log(name);
-    },[name]); // 2nd arg is a dependency array that controls when useEffect runs
+        fetch('http://localhost:8000/blogs')
+            .then(response => {   // this response is not the data; you need to do something to the object to get the data
+                return response.json();  // this json() function also takes some time to resolve, so you need another then() to wait for it to resolve
+            })
+            .then((data) => {  // at this point the data will be available
+                console.log(data);
+                setBlogs(data);
+            },[]);
+
+    },[]); // 2nd arg is a dependency array that controls when useEffect runs
                // an empty array as 2nd arg stops useEffect from running after the initial render
                // 2nd arg with a var inside the array causes useEffect to watch for changes in that variable and call itself when a change occurs
     return ( 
@@ -57,9 +55,7 @@ const Home = () => {
             ))
             */
             }
-            <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete}/>
-            <button onClick={() => setName('Phil')}>Change Name</button>
-            <p>{name}</p>
+            {blogs && <BlogList blogs={blogs} title="All Blogs" />}
         </div>
     );
 }
